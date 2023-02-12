8acca98ba0fbbc6f25ce30097a87944420d5dabd
PaplomatasP <p.paplomatas@hotmail.com>
2023-02-12 18:27:36 +0100

    ok9

--- scgenes/Scripts/pathway.R
+++ scgenes/Scripts/pathway.R
@@ -3,25 +3,29 @@ plot_pathview <- function(..., save_image = FALSE) {
   msg <- capture.output(pathview::pathview(...), type = "message")
   msg <- grep("image file", msg, value = T)
   filename <- sapply(strsplit(msg, " "), function(x) x[length(x)])
-
+  
+  cat("filename:", filename, "\n")
+  
   # Check if the file exists
   if (!file.exists(filename)) {
     stop("File does not exist:", filename)
   }
-  cat("filename:", filename, "\n")
-
+  
   # Save the image file to a temporary directory on the server
   tmp_dir <- tempdir()
   img_file <- file.path(tmp_dir, filename)
   file.copy(filename, img_file)
+  
   cat("img_file:", img_file, "\n")
+  
   # Use the full path to the image file
   img <- png::readPNG(img_file)
   grid::grid.raster(img)
-
+  
   # Optionally remove the image file if save_image is set to FALSE
   if(save_image == FALSE) file.remove(img_file)
 }
+
 
 
 
