cd292083ea6f8f5c5212a02c5e2dcc84a242d55e
PaplomatasP <p.paplomatas@hotmail.com>
2023-02-13 06:51:32 +0100

    Pathway try 15

--- bioc_pkg.R
+++ bioc_pkg.R
@@ -1,6 +1,6 @@
 InstallBioc=function(){
     
-  BiocManagerPackages=c("M3Drop","ComplexHeatmap","pathview","ensembldb","celldex",
+  BiocManagerPackages=c("M3Drop","ComplexHeatmap","pathview","ensembldb","celldex","org.Mm.eg.db",
                         "AnnotationFilter","AnnotationDbi","twoddpcr","EnsDb.Mmusculus.v79",
                         "Biobase","BiocFileCache","BiocGenerics","BiocParallel","BiocStyle",
                         "SingleR","STRINGdb","MAST" ,  "DESeq2")
