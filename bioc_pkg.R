962eb9aa30de91cb4907fb7de479b4da5ec3b1aa
Petros Paplomatas <84085141+PaplomatasP@users.noreply.github.com>
2024-04-07 12:50:14 +0200

    Update bioc_pkg.R

--- bioc_pkg.R
+++ bioc_pkg.R
@@ -4,7 +4,7 @@ InstallBioc=function(){
   BiocManagerPackages=c("M3Drop","ComplexHeatmap","pathview","ensembldb","celldex","org.Mm.eg.db",
                         "AnnotationFilter","AnnotationDbi","twoddpcr","EnsDb.Mmusculus.v79","EnsDb.Hsapiens.v79",
                         "Biobase","BiocFileCache","BiocGenerics","BiocParallel","BiocStyle",
-                        "SingleR","STRINGdb","MAST" ,  "DESeq2")
+                        "SingleR","STRINGdb","MAST" ,"scran","DESeq2")
 
 
   for ( Biopackage in BiocManagerPackages){
